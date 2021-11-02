8ca163f2924f273ba26f602a253a87f939256495
FireBallDevelopment <inovomiast@hotmail.com>
2021-11-02 19:29:45 +0100

    Finished the IShortner base!

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+//Link Shortner Script by: INovomiast
+
+const gotobtn = document.getElementById('gotobtn');
+const gotourl = document.getElementById('gotobtn_url');
+var timeleft = 5;
+const view_ad_btn = document.getElementById('');
+const donate_btn = document.getElementById('');
+const body = document.body;
+
+const urls = ['https://inovomiast.github.io/school-works/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx212dq1xz4gb2/?theme=light'];
+
+//URL Parameters
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const shid = urlParams.get('shid');
+
+if(shid === 'TM7eWuTD'){
+    document.title = 'SchoolWorks - IShortner';
+    body.onload = () => {
+        gotourl.href = '';
+        gotobtn.disabled = true;
+        var unlockTimer = setInterval(function(){
+            
+            gotobtn.innerHTML = timeleft + "secs To Unlock the Link!";
+            timeleft -= 1;
+            
+            if(timeleft === 0){
+                gotobtn.innerHTML = 'Access to Link';
+                clearInterval(unlockTimer);
+                gotobtn.disabled = false;
+                gotourl.href = urls[0];
+            }
+        }, 1000);
+        
+    }
+}else if(shid === 'wICEtQgO'){
+    document.title = 'Actitud y Aptitud - IShortner';
+    body.onload = () => {
+        gotourl.href = '';
+        gotobtn.disabled = true;
+        var unlockTimer = setInterval(function(){
+            
+            gotobtn.innerHTML = timeleft + "secs To Unlock the Link!";
+            timeleft -= 1;
+            
+            if(timeleft === 0){
+                gotobtn.innerHTML = 'Access to Link';
+                clearInterval(unlockTimer);
+                gotobtn.disabled = false;
+                gotourl.href = urls[1];
+            }
+        }, 1000);
+        
+    } 
+}
