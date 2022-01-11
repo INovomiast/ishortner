5c9c10cd04b84364d1f492d5d9c49f9fc5b05dc8
INovomiast <inovomiast@hotmail.com>
2022-01-11 10:02:25 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ const view_ad_btn = document.getElementById('');
 const donate_btn = document.getElementById('');
 const body = document.body;
 
-const urls = ['https://inovomiast.github.io/school-works/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx212dq1xz4gb2/?theme=light', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx274w1xz4u00/?theme=light', 'https://inovomiast.github.io/school-works/works/subjects/lengua/1fx21q01xz49vk/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx27il1xz4bpv/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx2d281xz4g8x/', 'https://inovomiast.github.io/school-works/works/subjects/filosofia/1fx2jwx1xz4r0a/', 'https://inovomiast.000webhostapp.com/user/inovomiast/cdn/school/audio/Audio_Telefono_Orientacion.mp3', 'https://inovomiast.github.io/school-works/works/subjects/lengua/1fx210kj1xz4t6g'];
+const urls = ['https://inovomiast.github.io/school-works/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx212dq1xz4gb2/?theme=light', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx274w1xz4u00/?theme=light', 'https://inovomiast.github.io/school-works/works/subjects/lengua/1fx21q01xz49vk/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx27il1xz4bpv/', 'https://inovomiast.github.io/school-works/works/subjects/oriprof/1fx2d281xz4g8x/', 'https://inovomiast.github.io/school-works/works/subjects/filosofia/1fx2jwx1xz4r0a/', 'https://inovomiast.000webhostapp.com/user/inovomiast/cdn/school/audio/Audio_Telefono_Orientacion.mp3', 'https://inovomiast.github.io/school-works/works/subjects/lengua/1fx210kj1xz4t6g', 'https://inovomiast.github.io/school-works/works/subjects/lengua/1fx2eo81xz4oq9', 'https://inovomiast.github.io/school-works/works/subjects/tecnologia/1fx2fht1xz4emd'];
 
 //URL Parameters
 const queryString = window.location.search;
@@ -174,6 +174,42 @@ body.onload = () => {
             clearInterval(unlockTimer);
             gotobtn.disabled = false;
             gotourl.href = urls[8];
+        }
+    
+    }, 1000);
+}
+}else if(shid === 'DVQv2iMp'){
+    document.title = 'La Situación en la España del Siglo XIX - IShortner';
+    body.onload = () => {
+        gotourl.href = '';
+        gotobtn.disabled = true;
+        var unlockTimer = setInterval(function(){                
+        gotobtn.innerHTML = timeleft + "secs To Unlock the Link!";
+        timeleft -= 1;
+                
+        if(timeleft === 0){
+            gotobtn.innerHTML = 'Access to Link';
+            clearInterval(unlockTimer);
+            gotobtn.disabled = false;
+            gotourl.href = urls[9];
+        }
+    
+    }, 1000);
+}
+}else if(shid === '0mzW3t2B'){
+    document.title = 'Hidraulica y Neumática - IShortner';
+    body.onload = () => {
+        gotourl.href = '';
+        gotobtn.disabled = true;
+        var unlockTimer = setInterval(function(){                
+        gotobtn.innerHTML = timeleft + "secs To Unlock the Link!";
+        timeleft -= 1;
+                
+        if(timeleft === 0){
+            gotobtn.innerHTML = 'Access to Link';
+            clearInterval(unlockTimer);
+            gotobtn.disabled = false;
+            gotourl.href = urls[10];
         }
     
     }, 1000);
